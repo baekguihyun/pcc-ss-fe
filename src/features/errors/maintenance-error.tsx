e46d439200c020a8b500a51abc1ff06f381e0d36
baekguihyun <beik2000@naver.com>
2025-03-12 20:14:11 +0900

    503 에러페이지 수정

--- src/features/errors/maintenance-error.tsx
+++ src/features/errors/maintenance-error.tsx
@@ -7,7 +7,7 @@ export default function MaintenanceError() {
         <h1 className='text-[7rem] font-bold leading-tight'>503</h1>
         <span className='text-2xl font-medium'>서버 점검 또는 휴식 중이에요</span>
         <p className='text-lg text-center text-muted-foreground'>
-          사용가능 시간은 오후 12시 ~ 오후 11시까지입니다. <br />
+          사용가능 시간은 오후 6시 ~ 오후 11시까지입니다. <br />
         </p>
         <div className='mt-6 flex gap-4'>
           <Button variant='outline' onClick={() => history.go(-1)}>
